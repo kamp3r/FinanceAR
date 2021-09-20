c77580664e913597c12c803e71402b961f5d5c7d
Martin Heredia <mhere92@gmail.com>
2021-09-20 04:18:54 -0300

    Beta 1, simulador JS, prueba en GITHUB

--- JS/main.js
+++ JS/main.js
@@ -1,62 +1,23 @@
-
-//inicio paso 1
-$("#siguiente1").on("click", () => {
-    $("#formSimulador0").animate({left: '-1000px'});
-    $("#formSimulador1").animate({left: '100px'});
-    $('#progreso').width("120px")
-    $('.paso1').css({ filter: "invert(0) sepia(8) saturate(30) hue-rotate(260deg)" , opacity: "1"})
-})
-$("#anterior0").on("click", () => {
-    $("#formSimulador0").animate({left: '100px'});
-    $("#formSimulador1").animate({left: '1000px'});
-    $('#progreso').width("-120px")
-    $('.paso1').css({ filter: "invert(0) sepia(1) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
-})
-
-//paso 1 a paso 2
-$("#siguiente2").on("click", () => {
-    $("#formSimulador1").animate({left: '-1000px'});
-    $("#formSimulador2").animate({left: '100px'});
-    $('#progreso').width("240px")
-    $('.paso2').css({ filter: "invert(0) sepia(8) saturate(30) hue-rotate(260deg)" , opacity: "1"})
-})
-$("#anterior1").on("click", () => {
-    $("#formSimulador1").animate({left: '100px'});
-    $("#formSimulador2").animate({left: '1000px'});
-    $('#progreso').width("120px")
-    $('.paso2').css({ filter: "invert(0) sepia(1) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
-})
-//paso 2 a paso 3
-$("#siguiente3").on("click", () => {
-    $("#formSimulador2").animate({left: '-1000px'});
-    $("#formSimulador3").animate({left: '100px'});
-    $('#progreso').width("360px")
-    $('.paso3').css({ filter: "invert(0) sepia(8) saturate(30) hue-rotate(260deg)" , opacity: "1"})
-})
-$("#anterior2").on("click", () => {
-    $("#formSimulador2").animate({left: '100px'});
-    $("#formSimulador3").animate({left: '1000px'});
-    $('#progreso').width("240px")
-    $('.paso3').css({ filter: "invert(0) sepia(1) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
-})
-//paso 3 a paso 4
-$("#siguiente4").on("click", () => {
-    $("#formSimulador3").animate({left: '-1000px'});
-    $("#formSimulador4").animate({left: '100px'});
-    $('#progreso').width("500px")
-    $('.paso4').css({ filter: "invert(0) sepia(8) saturate(30) hue-rotate(260deg)" , opacity: "1"})
-})
-
-//
-
 //Array datosClientes - Class Clientes
+
 const datosClientes = [];
 
 class Clientes {
 	constructor() {
 		this.client = {};
 	}
-	creacionClientes(nombre, apellido, email, empleo, antiguedad, sueldo, monto, plazo, tasas, cuota) {
+	creacionClientes(
+		nombre,
+		apellido,
+		email,
+		empleo,
+		antiguedad,
+		sueldo,
+		monto,
+		plazo,
+		tasas,
+		cuota
+	) {
 		this.client = {
 			id: Date.now(),
 			nombre: nombre,
@@ -68,11 +29,10 @@ class Clientes {
 			monto: monto,
 			plazo: plazo,
 			tasas: tasas,
-			cuota: cuota,
+			cuota: cuota
 		};
 		datosClientes.push(this.client);
-		document.forms[0].reset();
-		localStorage.setItem("Datos de Clientes", JSON.stringify(datosClientes));
+		localStorage.setItem("cliente", JSON.stringify(datosClientes));
 	}
 	calcularCuotas(monto, tasas, plazos) {
 		let hoy = new Date();
@@ -95,7 +55,7 @@ class Clientes {
 
 			return dia + "/" + mesIndex + "/" + año;
 		}
-        //recorrido meses para la tabla
+		//recorrido meses para la tabla
 		for (let i = 1; i <= plazos; i++) {
 			pagosIntereses = parseFloat(monto * (tasas / 100));
 			pagoAmortizacion = parseFloat(pagoMensual - pagosIntereses);
@@ -111,17 +71,23 @@ class Clientes {
 		}
 	}
 }
+
 var client = new Clientes();
 
 const datosTabla = document.querySelector("#tftable, tbody");
+
 //Variar valores de tasas
 const tasas = cambiarValorTasas();
 function cambiarValorTasas() {
 	let plazosOpc = $("#menuPlazos").prop("selectedIndex");
 	let opciones = $("#menuPlazos").prop("options");
-    if (opciones[plazosOpc].value == "0") {
-		$("#tasasSeleccion").attr("placeholder", "TEM(Tasa Efectiva Mensual)").val("").focus().blur();
-	} else if(opciones[plazosOpc].value == 12) {
+	if (opciones[plazosOpc].value == "0") {
+		$("#tasasSeleccion")
+			.attr("placeholder", "TEM(Tasa Efectiva Mensual)")
+			.val("")
+			.focus()
+			.blur();
+	} else if (opciones[plazosOpc].value == 12) {
 		parseFloat($("#tasasSeleccion").attr("placeholder", 3.95));
 	} else if (opciones[plazosOpc].value == 24) {
 		parseFloat($("#tasasSeleccion").attr("placeholder", 4.19));
@@ -133,27 +99,15 @@ function cambiarValorTasas() {
 
 $(document).on("change", cambiarValorTasas);
 
-;
-
-
-//Sincronia Input Range/Input Valor Prestamo
-$(document).on("change", "#inputRange", function (e) {
-	e.preventDefault();
-	$("#aSolicitar").val($(this).val());
-});
-$(document).on("change", "#aSolicitar", function (e) {
-	e.preventDefault();
-	$("#inputRange").val($(this).val());
-});
 //Eventos boton Calc
 $(".botonCalc").on("click", (e) => {
 	e.preventDefault();
-	client.calcularCuotas($("#aSolicitar").val(), parseFloat($("#tasasSeleccion").attr("placeholder")), $("#menuPlazos").val());
-});
+	client.calcularCuotas(
+		$("#aSolicitar").val(),
+		parseFloat($("#tasasSeleccion").attr("placeholder")),
+		$("#menuPlazos").val()
+	);
 
-//Eventos boton enviar
-$("#botonEnviar").on("click", (e) => {
-	e.preventDefault();
 	client.creacionClientes(
 		$("#nombre").val(),
 		$("#apellido").val(),
@@ -166,23 +120,60 @@ $("#botonEnviar").on("click", (e) => {
 		parseFloat($("#tasasSeleccion").attr("placeholder")),
 		parseFloat($(".valorCuota").text()).toFixed(2)
 	);
-
-    function generatePdf() {
-        var doc = new jspdf.jsPDF();
-        var offsetY = 4.797777777777778;
-        var lineHeight = 6.49111111111111;
-        var fontSize = 12;
-        doc.text(85, 10, "Tabla de Prestamo");
-        doc.autoTable({startY: 15,html: '.tftable', styles : { halign : 'center'}, headStyles :{fillColor : [124, 95, 240]}, alternateRowStyles: {fillColor : [231, 215, 252]}, tableLineColor: [124, 95, 240], tableLineWidth: 0.1,});
-        doc.setFontSize(fontSize);
-        var img = new Image();
-        img.src = 'images/signaturePDF.png'
-        doc.addImage(img, 'png', 100, doc.autoTable.previous.finalY + lineHeight * 1.5 + offsetY, 20, 20)
-        doc.text(90, doc.autoTable.previous.finalY + lineHeight * 5 + offsetY, "Juan Jose Urquiza")
-        doc.text(89, doc.autoTable.previous.finalY + lineHeight * 6 + offsetY, "Gerente FinanceAR")
-        doc.save ("detallePrestamo.pdf");
-
-    }
-    generatePdf();      
- 
 });
+
+
+//Eventos boton enviar
+$("#botonEnviar").on("click", function(e){
+	e.preventDefault()
+	function generatePdf() {
+		var doc = new jspdf.jsPDF();
+		var offsetY = 4.797777777777778;
+		var lineHeight = 6.49111111111111;
+		var fontSize = 12;
+		doc.text(85, 10, "Tabla de Prestamo");
+		doc.autoTable({
+			startY: 15,
+			html: ".tftable",
+			styles: { halign: "center" },
+			headStyles: { fillColor: [124, 95, 240] },
+			alternateRowStyles: { fillColor: [231, 215, 252] },
+			tableLineColor: [124, 95, 240],
+			tableLineWidth: 0.1,
+		});
+		doc.setFontSize(fontSize);
+		var img = new Image();
+		img.src = "images/signaturePDF.png";
+		doc.addImage(
+			img,
+			"png",
+			100,
+			doc.autoTable.previous.finalY + lineHeight * 1.5 + offsetY,
+			20,
+			20
+		);
+		doc.text(
+			90,
+			doc.autoTable.previous.finalY + lineHeight * 5 + offsetY,
+			"Juan Jose Urquiza"
+		);
+		doc.text(
+			89,
+			doc.autoTable.previous.finalY + lineHeight * 6 + offsetY,
+			"Gerente FinanceAR"
+		);
+		doc.save("detallePrestamo.pdf");
+	}
+	generatePdf();
+	const URL = 'https://jsonplaceholder.typicode.com/users'
+	$.ajax({
+		type: "POST",
+		url: URL,
+		data: client,
+		success: function(result){     
+			console.log(result);
+		}
+	});
+	$('form').trigger("reset")
+})
+
---
+++ JS/validacionAnimaciones.js
@@ -0,0 +1,112 @@
+//Scroll
+function animarLinks (link, haciaDonde){
+	link.click(function (e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: haciaDonde.offset().top}, 2000)
+		})
+}
+animarLinks($('#linkInicio'), $("#inicio"));
+animarLinks($("#linkConfiar"), $("#confiarEn"));
+animarLinks($("#linkServicios"), $("#servicios"));
+animarLinks($("#linkSimulador"), $("#contenedorSimulador"));
+
+//paso 1 a 2
+function nextBackFormulario(boton,primerFormulario,segundoFormulario,width,paso,saturacion,valorOpacidad){
+	boton.on("click", () => {
+	primerFormulario.toggleClass('active animacionIzq');
+	segundoFormulario.toggleClass('active');
+    $('#progreso').width(width)
+    paso.css({ filter: saturacion , opacity: valorOpacidad})
+})
+}
+
+nextBackFormulario($("#siguiente1"), $("#paso-1"),$("#paso-2"),"23%",$('.paso1'),"invert(0) sepia(8) saturate(30) hue-rotate(260deg)" ,"1")
+nextBackFormulario($("#anterior0"), $("#paso-1"),$("#paso-2"),'-23%',$('.paso1'),"invert(0) sepia(8) saturate(0) hue-rotate(260deg)","0.9")
+
+
+//Paso 2 en adelante con validaciones
+
+$("#paso-2").validate({
+	rules: {
+		nombre: { required: true, minlength: 2},
+		apellido: { required: true, minlength: 2},
+		mail: { required:true, email: true},
+	},
+	messages: {
+		nombre: "El campo es obligatorio.",
+		apellido: "El campo es obligatorio.",
+		mail : "El campo es obligatorio y debe tener formato de email correcto.",
+	}
+});
+
+function formulariosValidados(boton,primerFormulario,segundoFormulario,width,paso,saturacion,valorOpacidad){
+	boton.on("click", () => {
+	if (!primerFormulario.valid()) return false;	
+	primerFormulario.toggleClass('active animacionIzq');
+	segundoFormulario.toggleClass('active');
+    $('#progreso').width(width)
+    paso.css({ filter: saturacion , opacity: valorOpacidad})
+})
+}
+
+formulariosValidados($("#siguiente2"), $('#paso-2'), $("#paso-3"),"50%",$('.paso2'),"invert(0) sepia(8) saturate(30) hue-rotate(260deg)","1")
+nextBackFormulario($("#anterior1"), $('#paso-2'),$("#paso-3"),"25%",$('.paso2'),"invert(0) sepia(8) saturate(0) hue-rotate(260deg)","0.9")
+
+//paso 3 a paso 4
+$("#paso-3").validate({
+	rules: {
+		empleo: { required: true, minlength: 2},
+		antiguedad: { required: true, number: true, minlength: 1},
+		sueldo: { required:true, number: true, minlength: 5},
+	},
+	messages: {
+		empleo: "El campo es obligatorio.",
+		antiguedad: "El campo es obligatorio.",
+		sueldo: "Coloca un valor valido, por encima de los 10000 pesos",
+	}
+});
+formulariosValidados($("#siguiente3"), $('#paso-3'), $("#paso-4"),"75%",$('.paso3'),"invert(0) sepia(8) saturate(30) hue-rotate(260deg)","1")
+nextBackFormulario($("#anterior2"), $('#paso-3'), $("#paso-4"),"50%",$('.paso3'),"invert(0) sepia(8) saturate(0) hue-rotate(260deg)","0.9")
+
+//paso 4 a 5
+$.validator.addMethod("valorNoIgual", function(value, element, arg){
+	return arg !== value;
+   }, "Valor debe ser diferente al default.");
+$("#paso-4").validate({
+	rules: {
+		monto: { required: true, minlength: 5},
+		menuPlazos: { required: true, valorNoIgual: "default"},
+	},
+	messages: {
+		monto: "El campo es obligatorio. Recorda que aprobamos prestamos hasta 500000",
+		menuPlazos: {valorNoIgual: "El campo es obligatorio."}
+	}
+});
+formulariosValidados($("#siguiente4"), $('#paso-4'), $("#paso-5"),"100%",$('.paso4'),"invert(0) sepia(8) saturate(30) hue-rotate(260deg)","1");
+
+$('#botonEnviar').on('click', () =>{
+	$('#paso-1').toggleClass('animacionIzq active')
+	$('#paso-2').toggleClass('animacionIzq')
+	$('#paso-3').toggleClass('animacionIzq')
+	$('#paso-4').toggleClass('animacionIzq')
+	$('#paso-5').toggleClass('active')
+	$('#progreso').width('0%')
+	$('.paso1').css({filter: "invert(0) sepia(8) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
+	$('.paso2').css({filter: "invert(0) sepia(8) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
+	$('.paso3').css({filter: "invert(0) sepia(8) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
+	$('.paso4').css({filter: "invert(0) sepia(8) saturate(0) hue-rotate(260deg)", opacity: "0.9"})
+}
+)
+
+function mostrarMenu(){
+	$('.navigationContainer').css({display: "flex"})
+	$('.navigationContainer').css({top: "0"})
+}
+function cerrarMenu(){
+	$('.navigationContainer').css({top: "-110%"})
+}
+
+$('.menuAbierto').on('click', mostrarMenu)
+$('.menuCerrado').on('click', cerrarMenu)
+$('.linkItem').on('click', cerrarMenu)
